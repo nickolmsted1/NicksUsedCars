8b8403c8b590959077b77a933ae499094a0d3434
Nicholas Olmsted <37194521+nickolmsted1@users.noreply.github.com>
2019-04-16 13:31:53 -0700

    Added Identity to project

--- NicksUsedCars/Startup.cs
+++ NicksUsedCars/Startup.cs
@@ -24,6 +24,7 @@ namespace NicksUsedCars
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -51,6 +52,9 @@ namespace NicksUsedCars
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc();
+
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
